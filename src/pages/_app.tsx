1aa43b0d8684ed593e60d0e1e92da43b27b20b18
dev2820 <second28200@gmail.com>
2023-05-22 15:37:15 +0900

    refactor/ remove amplify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,5 @@
 import { RecoilRoot } from "recoil";
-import { Amplify } from "aws-amplify";
 import { AppProps } from "next/app";
-import awsconfig from "@/aws-exports";
-
-Amplify.configure(awsconfig);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/[username].tsx
+++ src/pages/users/[username].tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { API } from "aws-amplify";
 import { useRouter } from "next/router";
 
 export default async function UserPage() {
   const { query = {} } = useRouter();
-  const posts = await API.get("post", "/post", {});
-  console.log(posts);
+
   return (
     <div>
       <h1>here is user page for {query.username || "???"}</h1>
