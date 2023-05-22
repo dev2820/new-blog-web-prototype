dbcc4432468f1c1ec697f0652e287ab3615dfaa7
dev2820 <second28200@gmail.com>
2023-05-22 20:10:42 +0900

    refactor/ remove debug code

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -1,7 +1,7 @@
 import { Client } from "@notionhq/client";
 
 const NOTION_API_KEY = process.env.NEXT_PUBLIC_NOTION_API_KEY;
-console.log(process.env);
+
 const notion = new Client({ auth: NOTION_API_KEY });
 
 export const getPageList = async (username: string) => {
