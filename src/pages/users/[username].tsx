5be3a80938078c5ddd9723150938ddc3a878b57f
dev2820 <second28200@gmail.com>
2023-05-22 14:57:02 +0900

    refactor/ return to no amplify

--- src/pages/users/[username].tsx
+++ src/pages/users/[username].tsx
@@ -1,8 +1,10 @@
 import React from "react";
+// import { Amplify, API } from "aws-amplify";
 import { useRouter } from "next/router";
 
 export default function UserPage() {
   const { query = {} } = useRouter();
+  // const posts = await API.get("post", "/post");
   return (
     <div>
       <h1>here is user page for {query.username || "???"}</h1>
