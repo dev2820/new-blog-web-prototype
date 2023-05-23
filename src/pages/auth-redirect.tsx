22a31745dc066b56d0f579b55110c538b77e980b
dev2820 <second28200@gmail.com>
2023-05-23 17:57:13 +0900

    fix/ redirect after render

--- src/pages/auth-redirect.tsx
+++ src/pages/auth-redirect.tsx
@@ -2,16 +2,19 @@ import { useRouter } from "next/router";
 import { redirect } from "next/navigation";
 import { useNotion } from "@/stores/notion";
 import { useUser } from "@/stores/user";
+import { useEffect } from "react";
 export default function AuthRedirectPage() {
   const user = useUser();
   const router = useRouter();
   const notion = useNotion();
   const code = router.query.code as string;
 
-  if (code) {
-    notion.setCode(code);
-    redirect(`/@${user.profile.name}`);
-  }
+  useEffect(() => {
+    if (code) {
+      notion.setCode(code);
+      redirect(`/@${user.profile.name}`);
+    }
+  }, [code, notion, user.profile.name]);
 
   return <h1>loading...</h1>;
 }
