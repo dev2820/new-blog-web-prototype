4244b055440eb7639709099938e1f63771381524
dev2820 <second28200@gmail.com>
2023-05-27 10:39:00 +0900

    update/ change path to server

--- src/utils/new-blog.ts
+++ src/utils/new-blog.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { ENV } from "@/constants";
 
 export const newBlogAPI = axios.create({
-  baseURL: ENV.NEW_BLOG_SERVER_URL,
+  baseURL: `${ENV.NEW_BLOG_SERVER_URL}/api`,
 });
