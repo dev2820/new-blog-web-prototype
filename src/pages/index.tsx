92951dafffe4e462066f3c12b77f97564efa21f3
dev2820 <second28200@gmail.com>
2023-06-01 14:23:22 +0900

    refactor/ add test code for authorization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import { useUser } from "@/stores/user";
 import Layout from "@/layouts/Layout";
+import { newBlogAPI } from "@/utils";
 
 export default function HomePage() {
+  const testFunc = async () => {
+    const response = await newBlogAPI.get("https://new-blog.store/api/user");
+    console.log(response);
+  };
   return (
     <Layout>
       <h1>Home</h1>
+      <button onClick={testFunc}>work if authorized</button>
     </Layout>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * as notion from "./notion";
+export { newBlogAPI } from "./new-blog";
