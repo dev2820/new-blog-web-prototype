f437fed9a8c79877ac3b3ff5341dedcede9b8d03
dev2820 <second28200@gmail.com>
2023-06-01 17:31:25 +0900

    refactor/ add debug code

--- src/utils/new-blog.ts
+++ src/utils/new-blog.ts
@@ -7,7 +7,7 @@ export const newBlogAPI = axios.create({
 newBlogAPI.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("new-blog-token");
-
+    console.log(token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
