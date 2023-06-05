66000757d6842c37cb0aa097c10590c099a88c80
dev2820 <second28200@gmail.com>
2023-06-05 14:56:34 +0900

    refactor/ add global css to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { RecoilRoot } from "recoil";
 import { AppProps } from "next/app";
+import "@/app/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
