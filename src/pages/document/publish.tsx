1066c360fa69d9e2df5c2eeb0830e978336cb426
dev2820 <second28200@gmail.com>
2023-06-06 14:26:59 +0900

    refactor/ add debug codes

--- src/pages/document/publish.tsx
+++ src/pages/document/publish.tsx
@@ -1,25 +1,43 @@
 import { newBlogAPI } from "@/utils";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Card from "@/components/Card";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function PublishPage() {
   const { query } = useRouter();
+  const [pageMeta, setPageMeta] = useState<any>({});
+  const [pageBlocks, setPageBlocks] = useState<any>([]);
   useEffect(() => {
     const { provider, id } = query;
-    fetchDocument(String(provider), String(id));
+    fetchDocument(String(provider), String(id), setPageMeta, setPageBlocks);
   }, [query]);
 
   return (
     <>
-      <h2>publish</h2>
-      <article></article>
+      <h2>publish - {getTitle(pageMeta)}</h2>
+      <article>{pageBlocks.map((block: any) => getElement(block))}</article>
     </>
   );
 }
 
-async function fetchDocument(provider: string, id: string) {
-  const doc = await newBlogAPI.get(`/user/document/${provider}/${id}`);
-  console.log(doc);
+async function fetchDocument(
+  provider: string,
+  id: string,
+  setPageMeta: Dispatch<SetStateAction<{}>>,
+  setPageBlocks: Dispatch<SetStateAction<{}>>
+) {
+  const { data } = await newBlogAPI.get(`/user/document/${provider}/${id}`);
+  const { meta, content } = data;
+  setPageMeta(meta);
+  setPageBlocks(content);
 }
+
+const getTitle = (meta: any) => {
+  if (!meta.properties) return "";
+  return meta.properties.title.title[0].text.content;
+};
+
+const getElement = (block: any) => {
+  console.log(block);
+  return <Card>wait</Card>;
+};
